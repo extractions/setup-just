d7b3a176e12891edd2dada74fe1eaf57c961f2c6
Simon Sorensen <hello@simse.io>
2023-02-10 21:19:20 +0000

    feat: Pass token directly to checkOrInstallTool

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import * as setup from "@extractions/setup-crate";
 
 async function main() {
   try {
-    process.env.GITHUB_TOKEN =
+    const githubToken =
       process.env.GITHUB_TOKEN || core.getInput("github-token");
 
     const versionSpec = core.getInput("just-version");
-    const tool = await setup.checkOrInstallTool({
-      owner: "casey",
-      name: "just",
-      versionSpec,
-    });
+    const tool = await setup.checkOrInstallTool(
+      {
+        owner: "casey",
+        name: "just",
+        versionSpec,
+      },
+      { auth: githubToken }
+    );
     core.addPath(tool.dir);
     core.info(`Successfully setup ${tool.name} v${tool.version}`);
   } catch (err) {
